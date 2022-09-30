7d35dc97070f8016aa2506fffe5b9d5978f6797b
Ekene C <clarencekene@outlook.com>
2022-09-30 03:43:31 +0100

    tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTests(TestCase):
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    # def test_post_page(self):
+    #     response = self.client.get("/post/")
+    #     self.assertEqual(response.status_code, 200)
+    
+    def test_newposts_page(self):
+        response = self.client.get("/new/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_askhn_page(self):
+        response = self.client.get("/ask-hn/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_showhn_page(self):
+        response = self.client.get("/show-hn/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_hnjobs_page(self):
+        response = self.client.get("/hn/jobs/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_hnpolls_page(self):
+        response = self.client.get("/hn/polls/")
+        self.assertEqual(response.status_code, 200)
--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("new", views.new_hn, name='new'),
-    path("post/<pk>", views.post, name='post'),
+    path("new/", views.new_hn, name='new'),
+    path("post/<pk>/", views.post, name='post'),
     path("ask-hn/", views.ask_hn, name='askhn'),
     path("show-hn/", views.show_hn, name='showhn'),
     path("hn/jobs/", views.jobs_hn, name='jobshn'),
