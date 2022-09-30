c2232faa2c96f3f96dc350931bb941e757aa613c
Ekene C <clarencekene@outlook.com>
2022-09-30 02:29:53 +0100

    test

--- qcproject/settings.py
+++ qcproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y_u5br+dd%juwm)ux#8(yb4c1k2h$5^#7i)55#jvd%q!d-tf5r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['railway.app']
+ALLOWED_HOSTS = ['127.0.0.1' ,'railway.app']
 
 
 # Application definition
